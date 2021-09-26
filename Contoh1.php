760ef9bd3786f60bd58e317678c76ff70730c027
tiaraaprillia <90761658+tiaraaprillia@users.noreply.github.com>
2021-09-26 20:17:11 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_controller
+{ 
+    public function index()
+    { 
+     "<h1>perkenalkan</h1>";
+    "Nama saya Tiara aprillia
+          saya tinggal di serang baru kab.bekasi 
+          olahraga yang saya sukai adalah Bulutangkis";
+    }
+}
