e6084854e68f409833626d1eafcd84ea84343018
tiaraaprillia <90761658+tiaraaprillia@users.noreply.github.com>
2021-09-29 19:37:21 +0700

    Add files via upload

---
+++ view-latihan1.php
@@ -0,0 +1,12 @@
+<html>
+     <head> <title>latihan1</title>
+     </head>
+
+ <body>
+        hallo kawan... yuk kita belajar web programing...!!!<br>
+        nilai 1 = <?= $nilai1; ?>
+        nilai 2 = <?= $nilai2; ?>
+        ini hasil dari permodelan dengan method penjumlahan yaitu <?= $nilai1 . "+" . $nilai2 ."=" .
+        $hasil; ?>
+</body>
+</html>
